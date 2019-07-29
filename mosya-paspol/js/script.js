7b5e627130cbeea1552e961bfa85d84665a49088
EC2 Default User <ec2-user@ip-172-31-35-244.ap-northeast-1.compute.internal>
2019-07-29 15:45:10 +0000

    20190730-01

--- mosya-paspol/js/script.js
+++ mosya-paspol/js/script.js
@@ -16,19 +16,42 @@ $('.js-backTop').click(function() {
 });
 
 // flexslider
-$(window).on('load resize', function() {
-    $(".flexslider").flexslider({
-        //       slideshowSpeed: 5000,
-        //       animation: "fade",
-        //       controlNav: false,
-        //       directionNav: false,
-        //       pauseOnHover: false,
-        //       direction: "horizontal",
-        //       reverse: false,
-        //       animationLoop: true,
-        //       animationSpeed: 1500,
-        //       prevText: "&lt;",
-        //       nextText: "&gt;",
-        //       slideshow: true
+$(window).on("load", function() {
+    $('.flexslider').flexslider({
+        slideshowSpeed: 5000,
+        animation: "fade",
+        controlNav: false,
+        directionNav: false,
+        pauseOnHover: false,
+        direction: "horizontal",
+        reverse: false,
+        animationSpeed: 1500,
+        prevText: "&lt;",
+        nextText: "&gt;",
+        slideshow: true
     });
+});
+
+// image Resize
+$(window).on("load resize", function() {
+    if ($(window).width() > 900) {
+        $(".js-mainVisual").css({ height: $(window).height() - 30 });
+    }
+    else {
+        $(".js-mainVisual").css({ height: $(window).height() + 20 });
+    }
+
+    outerWidth = $(".js-mainVisual").width();
+    outerHeight = $(".js-mainVisual").height();
+    innerWidth = $(".flexslider").width();
+    innerHeight = $(".flexslider").height();
+    widthRatio = outerWidth / innerWidth;
+    heightRatio = outerHeight / innerHeight;
+    maxRatio = Math.max(widthRatio, heightRatio);
+    zoomedWidth = Math.floor(innerWidth * maxRatio);
+    zoomedHeight = Math.floor(innerHeight * maxRatio);
+    topMargin = Math.floor((zoomedHeight - outerHeight) / -2);
+    leftMargin = Math.floor((zoomedWidth - outerWidth) / -2);
+    $(".flexslider").css({ width: zoomedWidth, height: zoomedHeight, top: topMargin, left: leftMargin });
+
 });
