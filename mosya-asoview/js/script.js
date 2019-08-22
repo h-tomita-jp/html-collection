10028236d972d1868caeffcabb7f661e3bbac1e1
EC2 Default User <ec2-user@ip-172-31-35-244.ap-northeast-1.compute.internal>
2019-08-22 06:23:15 +0000

    20190822-01

--- mosya-asoview/js/script.js
+++ mosya-asoview/js/script.js
@@ -1,26 +1,5 @@
-// .switch img name replace
-// $(function(){
-//   var $setElem = $('.switch'),
-//       pcName = '_pc',
-//       spName = '_sp',
-//       replaceWidth = 768;
-
-//   $setElem.each(function(){
-//     var $this = $(this);
-//     function imgSize(){
-//       var windowWidth = parseInt(window.innerWidth);
-//       if(windowWidth >= replaceWidth) {
-//         $this.attr('src',$this.attr('src').replace(spName,pcName));
-//       } else if(windowWidth < replaceWidth) {
-//         $this.attr('src',$this.attr('src').replace(pcName,spName));
-//       }
-//     }
-//     $(window).on('load resize', function(){imgSize();});
-//   });
-// });
-
 $(function() {
-  //tab
+  //tab切り替え
   $('ul.tab li').click(function() {
     var index = $('ul.tab li').index(this);
     if(index > 1){index -= 2;}
@@ -34,13 +13,7 @@ $(function() {
   $('.plan_set a .text').matchHeight();
 });
 
-// function option_change(){
-//   var link = document.sel2_form.sel2.value;
-//   var a = '<a href="'+link+'"target="_blank">プランを見る</a>';
-//   $('.select_wrap + .btn').empty();
-//   $('.select_wrap + .btn').html(a);
-// }
-
+// ページトップのFLEXSLIDER処理
 $('.bg_slider,.bg_slider_text').flexslider({
     animation: "fade",
     slideshowSpeed: 3500,
@@ -49,37 +22,7 @@ $('.bg_slider,.bg_slider_text').flexslider({
     directionNav: false,
 });
 
-// $(window).on('load resize', function() {
-//     $('.bg_slider').addClass('open');
-//     var bgH = $('.bg_area .bg02').height();
-// });
-
-// //kv周りの動き
-// $(function() {
-//     setTimeout(function() {
-//         $('.bg_area .bg').addClass('open');
-//         setTimeout(function() {
-//             $('.bg_area .bg,.move_text').addClass('sec');
-//             var i = 0;
-//             setTimeout(function() {
-//                 $('.main_title span').each(function() {
-//                     $(this).delay(500 * i).queue(function() {
-//                         $(this).addClass('delay');
-//                     });
-//                     i++;
-//                 });
-//                 setTimeout(function() {
-//                     $('.bg_slider_text').find('span').addClass('delay');
-//                 }, 1500);
-//             }, 1000);
-//             $('.bg h2').addClass('show');
-//             setTimeout(function() {
-//                 $('.text,.balloon').addClass('show');
-//             }, 500);
-//         }, 300);
-//     }, 500);
-// });
-
+// ページスクロールの結果、該当DIVに到達したらアニメーション開始
 $(window).scroll(function() {
     var about = $('#about_area').offset().top;
     var aboutH = about - $(window).height();
