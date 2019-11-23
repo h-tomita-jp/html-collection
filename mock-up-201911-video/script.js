2cbc99c4a5229d795d5b7791424ff60342e099fc
EC2 Default User <ec2-user@ip-172-31-35-244.ap-northeast-1.compute.internal>
2019-11-23 16:51:41 +0000

    20191124-0151

--- mock-up-201911-video/script.js
+++ mock-up-201911-video/script.js
@@ -29,34 +29,34 @@ document.onkeydown = keydown;
 
 function keydown() {
   if (event.keyCode == 49 || event.keyCode == 97) {
-    video2.style.display = 'none';
-    video3.style.display = 'none';
-    video4.style.display = 'none';
-    video1.style.display = 'block';
+    video2.style.opacity = 0;
+    video3.style.opacity = 0;
+    video4.style.opacity = 0;
+    video1.style.opacity = 1;
     video1.currentTime = 0;
     video1.play();
   }
   if (event.keyCode == 50 || event.keyCode == 98) {
-    video1.style.display = 'none';
-    video3.style.display = 'none';
-    video4.style.display = 'none';
-    video2.style.display = 'block';
+    video1.style.opacity = 0;
+    video3.style.opacity = 0;
+    video4.style.opacity = 0;
+    video2.style.opacity = 1;
     video2.currentTime = 0;
     video2.play();
   }
   if (event.keyCode == 51 || event.keyCode == 99) {
-    video1.style.display = 'none';
-    video2.style.display = 'none';
-    video4.style.display = 'none';
-    video3.style.display = 'block';
+    video1.style.opacity = 0;
+    video2.style.opacity = 0;
+    video4.style.opacity = 0;
+    video3.style.opacity = 1;
     video3.currentTime = 0;
     video3.play();
   }
   if (event.keyCode == 52 || event.keyCode == 100) {
-    video1.style.display = 'none';
-    video2.style.display = 'none';
-    video3.style.display = 'none';
-    video4.style.display = 'block';
+    video1.style.opacity = 0;
+    video2.style.opacity = 0;
+    video3.style.opacity = 0;
+    video4.style.opacity = 1;
     video4.currentTime = 0;
     video4.play();
   }
