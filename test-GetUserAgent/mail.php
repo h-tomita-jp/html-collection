2b919fd71efd9b45f6e7c302d4447644bffd48f6
EC2 Default User <ec2-user@ip-172-31-35-244.ap-northeast-1.compute.internal>
2019-12-11 08:50:59 +0000

    20191211-1750

--- test-GetUserAgent/mail.php
+++ test-GetUserAgent/mail.php
@@ -1,14 +1,18 @@
 <?php
+
+//URLパラメタ(ua)を取得
+if(isset($_GET['ua'])) { $ua = $_GET['ua']; }
+
 //メールの情報を設定
 $mailto = "gfa00161@nifty.com";
-$title = "タイトルテスト";
-$message = "本文のテストです。";
-$option= "From: h.tomita.jp@gmail.com";
+$title = "UserAgent";
+$message = "UserAgent: ".$ua;
+$option= "From: gfa00161@nifty.com";
 
 //メールの送信
 if(mail($mailto,$title,$message,$option)){
-	echo "送信成功";
+	echo "ユーザエージェント情報のメール送信成功";
 }else{
-	echo "送信失敗";
+	echo "ユーザエージェント情報のメール送信失敗";
 }
 ?>
